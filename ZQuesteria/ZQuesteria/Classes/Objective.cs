ccc9b9b32562899203bd2223c7c09f4429cb1206
ZinoZack47 <56520486+ZinoZack47@users.noreply.github.com>
2022-02-24 11:21:42 +0100

    Add project files.

---
+++ ZQuesteria/ZQuesteria/Classes/Objective.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+enum EState
+{
+    STATUS_NULL = 0,
+    STATUS_CURRENT,
+    STATUS_OK,
+    STATUS_FAILED
+}
+
+namespace ZQuesteria.Classes
+{
+    internal class Objective
+    {
+        Objective(string szTitle, int iPosition = 0, bool isOptional = false)
+        {
+            this.szTitle = szTitle;
+            this.iPosition = iPosition;
+            this.isOptional = isOptional;
+        }
+
+        public EState iState = EState.STATUS_NULL;
+        private int iPosition;
+        private bool isOptional;
+        private string szTitle;
+    }
+}
---
+++ ZQuesteria/ZQuesteria/Classes/Quest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+enum EQuestType
+{
+    QUESTTYPE_MAIN = 0,
+    QUESTTYPE_SIDE,
+    QUESTTPE_MISC
+}
+
+namespace ZQuesteria.Classes
+{
+    internal class Quest
+    {
+        Quest(string szTitle, EQuestType Type)
+        {
+            this.szTitle = szTitle;
+            this.Type = Type;
+        }
+        public void AddObjective(Objective objective)
+        {
+            liObjectives.Add(objective);
+        }
+
+        private string szTitle;
+        private EQuestType Type;
+        private List<Objective> liObjectives = new List<Objective>();
+    }
+}
